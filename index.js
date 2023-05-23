03897587aceb6cbad1f80233b9e3fb1aa7602863
FM842mnshagaki <129007927+FM842mnshagaki@users.noreply.github.com>
2023-05-23 11:51:27 +0900

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const form = document.getElementById("form");
-const input = document.getElementById("input");
-const ul = document.getElementById("ul");
-
-form.addEventListener("submit",function(event) {
-    event.preventDefault();
-  console.log(input.value);
-})
-
