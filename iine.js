b503e86e925d4f9affb94fdea39b3603a9f43a17
abudabud <129007927+FM842mnshagaki@users.noreply.github.com>
2023-07-10 22:00:52 +0900

    Update iine.js

--- iine.js
+++ iine.js
@@ -1,14 +1,24 @@
-function textcopy() {
-    var textarea = document.getElementById("textarea").value;
-    document.getElementById("textarea").value = "";
-    var counter = 0;
-    textarea = "<div class='frame'>" + "<p class='memo'>" + textarea + "</p>" +
-      "<button type='button' + class='far fa-thumbs-up' id='goodbutton'>" +
-      "いいね" + "</button>" + "<a id='clicks'>" + counter + "</a>" +
-      "</div>" + document.getElementById("insert").innerHTML;
-    document.getElementById("insert").innerHTML = textarea;
-    document.getElementById("goodbutton").onclick = function() {
-      counter += 1;
-      document.getElementById("clicks").innerHTML = counter;
-    }
+document.addEventListener('DOMContentLoaded', function() {
+  var likeButton = document.getElementById('likeButton');
+  var likeCount = document.getElementById('likeCount');
+  var videoId = likeButton.getAttribute('data-video-id');
+
+  likeButton.addEventListener('click', function() {
+    likeVideo(videoId);
+  });
+
+  function likeVideo(videoId) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/like', true);
+    xhr.setRequestHeader('Content-type', 'application/json');
+
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        var response = JSON.parse(xhr.responseText);
+        likeCount.textContent = response.likes;
+      }
+    };
+
+    xhr.send(JSON.stringify({ videoId: videoId }));
   }
+});
